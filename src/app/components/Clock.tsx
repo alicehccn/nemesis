23d2db98fd5124f65fb192bb2f602db99fd9d042
Alice Huang <alicehccn@gmail.com>
2026-04-22 12:50:19 -0700

    initial time

--- src/app/components/Clock.tsx
+++ src/app/components/Clock.tsx
@@ -2,9 +2,10 @@ import { CSSProperties, useEffect, useRef, useState } from "react";
 import { CLOCK_INTERVAL, CLOCK_NUMERALS } from "../constants";
 
 const Clock: React.FC = () => {
-  const [hour, setHour] = useState<number>(new Date().getHours());
-  const [minute, setMinute] = useState<number>(new Date().getMinutes());
-  const [second, setSecond] = useState<number>(new Date().getSeconds());
+  const today = new Date();
+  const [hour, setHour] = useState<number>(today.getHours());
+  const [minute, setMinute] = useState<number>(today.getMinutes());
+  const [second, setSecond] = useState<number>(today.getSeconds());
 
   function useInterval(callback: () => void, delay: number) {
     const savedCallback = useRef(callback);
@@ -25,10 +26,11 @@ const Clock: React.FC = () => {
   }
 
   useInterval(() => {
-    if (minute !== new Date().getMinutes()) {
-      setHour(new Date().getHours());
-      setMinute(new Date().getMinutes());
-      setSecond(new Date().getSeconds());
+    const now = new Date();
+    if (minute !== now.getMinutes()) {
+      setHour(now.getHours());
+      setMinute(now.getMinutes());
+      setSecond(now.getSeconds());
     }
   }, CLOCK_INTERVAL);
 
